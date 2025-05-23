fda007634f5c06ad009a56d9fab8b93086d995e6
MalLunBar <malinelundgren1991@gmail.com>
2025-05-23 17:57:52 +0200

    styling empty state

--- src/components/EmptyState.tsx
+++ src/components/EmptyState.tsx
@@ -1,8 +1,8 @@
 export const EmptyState = () => {
   return (
-    <div className="text-center mt-10 text-gray-500">
-        <p className="text-lg">No tasks yet</p>
-        <p className="text-sm">Click “New Task +” to get started</p>
+    <div className="bg-white opacity-80 p-12 text-center rounded-2xl mt-8 text-black">
+        <p className="text-xl">No tasks yet</p>
+        <p className="text-md">Click “New Task +” to get started</p>
       </div>
   )
 }
