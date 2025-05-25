5e3a14ba2b779cc85b374bb47f0b87bad7e3cc70
MalLunBar <malinelundgren1991@gmail.com>
2025-05-25 23:51:51 +0200

    lighthouse fix

--- src/components/HeroMessage.tsx
+++ src/components/HeroMessage.tsx
@@ -12,7 +12,7 @@ export const HeroMessage = () => {
           className="w-6 h-auto" />
       </header>
       <div className="flex flex-col-reverse py-10 px-12 sm:py-24 sm:px-20 max-w-[400px] sm:max-w-[500px] md:max-w-[600px] m-auto mx-auto lg:min-w-[500px] lg:px-12 lg:py-40">
-        <h1 className="font-medium text-3xl sm:text-5xl md:text-6xl">From Clutter to <span className="text-[#FFBB00] bg-gray-300 px-[3px] rounded-4xl">Done</span> with Done-ly</h1>
+        <h1 className="font-medium text-3xl sm:text-5xl md:text-6xl">From Clutter to <span className="text-[#FFC116] bg-black px-2 rounded-full">Done</span> with Done-ly</h1>
         <p className="font-light text-base sm:text-xl">Make it simple</p>
 
       </div>
